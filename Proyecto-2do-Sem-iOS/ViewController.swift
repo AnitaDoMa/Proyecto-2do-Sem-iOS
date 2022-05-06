04aa96206c6655933508dcddac2409062256853b
me.ana <consuelo.dominguezm@uanl.edu.mx>
2022-05-06 14:47:12 -0500

    Idkkk

--- Proyecto-2do-Sem-iOS/ViewController.swift
+++ Proyecto-2do-Sem-iOS/ViewController.swift
@@ -9,11 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+   
+    
+    
 }
+
+
 
