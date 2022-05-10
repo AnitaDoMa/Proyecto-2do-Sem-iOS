c0299956d60fb994a99ea2bb65b22ad0df4ac15b
me.ana <consuelo.dominguezm@uanl.edu.mx>
2022-05-10 15:13:25 -0500

    idkkkkkk

--- Proyecto-2do-Sem-iOS/ViewController.swift
+++ Proyecto-2do-Sem-iOS/ViewController.swift
@@ -8,20 +8,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
     
 
-    
+    let transparentView = UIView()
     
     override func viewDidLoad() {
-        super.viewDidLoad()
+    super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-   
     
+
+    }
     
-}
-
-
 
