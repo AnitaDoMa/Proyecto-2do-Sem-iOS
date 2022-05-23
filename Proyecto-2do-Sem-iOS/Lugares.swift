98b50867202466ae1c3f5e1e9dac64e8e863dbab
iOSLab10 <ioslab10@iOSLab10s-MacBook-Pro.local>
2022-05-23 16:31:37 -0500

    Lugares 2

--- Proyecto-2do-Sem-iOS/Lugares.swift
+++ Proyecto-2do-Sem-iOS/Lugares.swift
@@ -12,6 +12,20 @@ class Lugares: UIViewController {
     
     @IBOutlet var Caadi: UILabel!
     @IBOutlet var HoCaadi: UILabel!
+    @IBOutlet var Tes: UILabel!
+
+    @IBOutlet var HTes: UILabel!
+    @IBOutlet var OrgDep: UILabel!
+    @IBOutlet var HOrgDep: UILabel!
+    @IBOutlet var DepBec: UILabel!
+    @IBOutlet var HDepBec: UILabel!
+    @IBOutlet var Rec: UILabel!
+    @IBOutlet var HRec: UILabel!
+    @IBOutlet var SerMed: UILabel!
+    @IBOutlet var HSerMed: UILabel!
+    @IBOutlet var LibUniv: UILabel!
+    @IBOutlet var HLibUniv: UILabel!
+    
     override func viewDidLoad() {
     super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -23,7 +37,47 @@ class Lugares: UIViewController {
         HoCaadi.layer.cornerRadius = 8
         HoCaadi.layer.borderColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
         HoCaadi.layer.borderWidth = 1
-        
+        Tes.layer.backgroundColor = #colorLiteral(red: 0.6238300204, green: 0.8544803858, blue: 0.9987178445, alpha: 1)
+        Tes.layer.cornerRadius = 8
+        Tes.layer.borderColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        Tes.layer.borderWidth = 1
+        HTes.layer.backgroundColor = #colorLiteral(red: 0.7725575566, green: 0.9285991192, blue: 1, alpha: 1)
+        HTes.layer.cornerRadius = 8
+        HTes.layer.borderWidth = 1
+        HTes.layer.borderColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        OrgDep.layer.backgroundColor = #colorLiteral(red: 0.6238300204, green: 0.8544803858, blue: 0.9987178445, alpha: 1)
+        OrgDep.layer.cornerRadius = 8
+        OrgDep.layer.borderColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        OrgDep.layer.borderWidth = 1
+        HOrgDep.layer.backgroundColor = #colorLiteral(red: 0.7725575566, green: 0.9285991192, blue: 1, alpha: 1)
+        HOrgDep.layer.cornerRadius = 8
+        HOrgDep.layer.borderWidth = 1
+        HOrgDep.layer.borderColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        DepBec.layer.backgroundColor = #colorLiteral(red: 0.6238300204, green: 0.8544803858, blue: 0.9987178445, alpha: 1)
+        DepBec.layer.cornerRadius = 8
+        DepBec.layer.borderColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        DepBec.layer.borderWidth = 1
+        HDepBec.layer.backgroundColor = #colorLiteral(red: 0.7725575566, green: 0.9285991192, blue: 1, alpha: 1)
+        HDepBec.layer.cornerRadius = 8
+        HDepBec.layer.borderWidth = 1
+        HDepBec.layer.borderColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        Rec.layer.backgroundColor = #colorLiteral(red: 0.6238300204, green: 0.8544803858, blue: 0.9987178445, alpha: 1)
+        Rec.layer.cornerRadius = 8
+        Rec.layer.borderColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        Rec.layer.borderWidth = 1
+        HRec.layer.backgroundColor = #colorLiteral(red: 0.7725575566, green: 0.9285991192, blue: 1, alpha: 1)
+        HRec.layer.cornerRadius = 8
+        HRec.layer.borderWidth = 1
+        HRec.layer.borderColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        SerMed.layer.backgroundColor = #colorLiteral(red: 0.6238300204, green: 0.8544803858, blue: 0.9987178445, alpha: 1)
+        SerMed.layer.cornerRadius = 8
+        SerMed.layer.borderColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        SerMed.layer.borderWidth = 1
+        HSerMed.layer.backgroundColor = #colorLiteral(red: 0.7725575566, green: 0.9285991192, blue: 1, alpha: 1)
+        HSerMed.layer.cornerRadius = 8
+        HSerMed.layer.borderWidth = 1
+        HSerMed.layer.borderColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+            
         
         
     }
