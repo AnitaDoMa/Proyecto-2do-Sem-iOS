2f3f0136b56bed132cccd4a47003d2bb99884311
iOSLab10 <ioslab10@iOSLab10s-MacBook-Pro.local>
2022-05-23 17:10:28 -0500

    h

--- Proyecto-2do-Sem-iOS/Lugares.swift
+++ Proyecto-2do-Sem-iOS/Lugares.swift
@@ -13,7 +13,6 @@ class Lugares: UIViewController {
     @IBOutlet var Caadi: UILabel!
     @IBOutlet var HoCaadi: UILabel!
     @IBOutlet var Tes: UILabel!
-
     @IBOutlet var HTes: UILabel!
     @IBOutlet var OrgDep: UILabel!
     @IBOutlet var HOrgDep: UILabel!
@@ -25,6 +24,18 @@ class Lugares: UIViewController {
     @IBOutlet var HSerMed: UILabel!
     @IBOutlet var LibUniv: UILabel!
     @IBOutlet var HLibUniv: UILabel!
+    @IBOutlet var Bib: UILabel!
+    @IBOutlet var HBib: UILabel!
+
+    
+    @IBOutlet var Cred: UILabel!
+    @IBOutlet var HCred: UILabel!
+    @IBOutlet var CenEv: UILabel!
+    @IBOutlet var HCenEv: UILabel!
+    @IBOutlet var CapAlf: UILabel!
+    @IBOutlet var HCapAlf: UILabel!
+    @IBOutlet var MesDir: UILabel!
+    @IBOutlet var HMesDir: UILabel!
     
     override func viewDidLoad() {
     super.viewDidLoad()
@@ -77,8 +88,53 @@ class Lugares: UIViewController {
         HSerMed.layer.cornerRadius = 8
         HSerMed.layer.borderWidth = 1
         HSerMed.layer.borderColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
-            
-        
-        
+        LibUniv.layer.backgroundColor = #colorLiteral(red: 0.6238300204, green: 0.8544803858, blue: 0.9987178445, alpha: 1)
+        LibUniv.layer.cornerRadius = 8
+        LibUniv.layer.borderColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        LibUniv.layer.borderWidth = 1
+        HLibUniv.layer.backgroundColor = #colorLiteral(red: 0.7725575566, green: 0.9285991192, blue: 1, alpha: 1)
+        HLibUniv.layer.cornerRadius = 8
+        HLibUniv.layer.borderWidth = 1
+        HLibUniv.layer.borderColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        Bib.layer.backgroundColor = #colorLiteral(red: 0.6238300204, green: 0.8544803858, blue: 0.9987178445, alpha: 1)
+        Bib.layer.cornerRadius = 8
+        Bib.layer.borderColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        Bib.layer.borderWidth = 1
+        HBib.layer.backgroundColor = #colorLiteral(red: 0.7725575566, green: 0.9285991192, blue: 1, alpha: 1)
+        HBib.layer.cornerRadius = 8
+        HBib.layer.borderWidth = 1
+        HBib.layer.borderColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        MesDir.layer.backgroundColor = #colorLiteral(red: 0.6238300204, green: 0.8544803858, blue: 0.9987178445, alpha: 1)
+        MesDir.layer.cornerRadius = 8
+        MesDir.layer.borderColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        MesDir.layer.borderWidth = 1
+        HMesDir.layer.backgroundColor = #colorLiteral(red: 0.7725575566, green: 0.9285991192, blue: 1, alpha: 1)
+        HMesDir.layer.cornerRadius = 8
+        HMesDir.layer.borderWidth = 1
+        HMesDir.layer.borderColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        Cred.layer.backgroundColor = #colorLiteral(red: 0.6238300204, green: 0.8544803858, blue: 0.9987178445, alpha: 1)
+        Cred.layer.cornerRadius = 8
+        Cred.layer.borderColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        Cred.layer.borderWidth = 1
+        HCred.layer.backgroundColor = #colorLiteral(red: 0.7725575566, green: 0.9285991192, blue: 1, alpha: 1)
+        HCred.layer.cornerRadius = 8
+        HCred.layer.borderWidth = 1
+        HCred.layer.borderColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        CapAlf.layer.backgroundColor = #colorLiteral(red: 0.6239831448, green: 0.8543829322, blue: 1, alpha: 1)
+        CapAlf.layer.cornerRadius = 8
+        CapAlf.layer.borderColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        CapAlf.layer.borderWidth = 1
+        HCapAlf.layer.backgroundColor = #colorLiteral(red: 0.7723222375, green: 0.9287762642, blue: 1, alpha: 1)
+        HCapAlf.layer.cornerRadius = 8
+        HCapAlf.layer.borderColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        HCapAlf.layer.borderWidth = 1
+        CenEv.layer.backgroundColor = #colorLiteral(red: 0.6078096032, green: 0.8388676047, blue: 0.9787705541, alpha: 1)
+        CenEv.layer.cornerRadius = 8
+        CenEv.layer.borderColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        CenEv.layer.borderWidth = 1
+        HCenEv.layer.backgroundColor = #colorLiteral(red: 0.7723222375, green: 0.9287762642, blue: 1, alpha: 1)
+        HCenEv.layer.cornerRadius = 8
+        HCenEv.layer.borderColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        HCenEv.layer.borderWidth = 1
     }
 }
