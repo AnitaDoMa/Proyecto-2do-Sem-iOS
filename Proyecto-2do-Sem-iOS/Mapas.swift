2ed47827b6cb24690b758339fc1d52e2f544f8f4
me.ana <consuelo.dominguezm@uanl.edu.mx>
2022-05-23 15:10:51 -0500

    Drop-down

--- Proyecto-2do-Sem-iOS/Mapas.swift
+++ Proyecto-2do-Sem-iOS/Mapas.swift
@@ -1,16 +1,26 @@
 //
-//  Mapas.swift
+//  Info.swift
 //  Proyecto-2do-Sem-iOS
 //
-//  Created by iOSLab14 on 20/05/22.
+//  Created by iOSLab14 on 18/05/22.
 //
 
 import UIKit
+import SwiftUI
 
 class Mapas: UIViewController {
+    
+    @IBOutlet weak var container: UIView!
     
     override func viewDidLoad() {
     super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let childView = UIHostingController(rootView: MapasUI())
+        addChild(childView)
+        childView.view.frame = container.bounds
+        container.addSubview(childView.view)
+        
+        
     }
 }
