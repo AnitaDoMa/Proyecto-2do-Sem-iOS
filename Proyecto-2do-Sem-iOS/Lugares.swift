017f545800347c65e1386a24b572f6d26ace5f47
iOSLab10 <ioslab10@iOSLab10s-MacBook-Pro.local>
2022-05-23 15:53:46 -0500

    Lugares

--- Proyecto-2do-Sem-iOS/Lugares.swift
+++ Proyecto-2do-Sem-iOS/Lugares.swift
@@ -8,9 +8,23 @@
 import UIKit
 
 class Lugares: UIViewController {
+
     
+    @IBOutlet var Caadi: UILabel!
+    @IBOutlet var HoCaadi: UILabel!
     override func viewDidLoad() {
     super.viewDidLoad()
         // Do any additional setup after loading the view.
+        Caadi.layer.backgroundColor = #colorLiteral(red: 0.6274509804, green: 0.8549019608, blue: 1, alpha: 1)
+        Caadi.layer.cornerRadius = 8
+        Caadi.layer.borderColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        Caadi.layer.borderWidth = 1
+        HoCaadi.layer.backgroundColor = #colorLiteral(red: 0.7725575566, green: 0.9285991192, blue: 1, alpha: 1)
+        HoCaadi.layer.cornerRadius = 8
+        HoCaadi.layer.borderColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        HoCaadi.layer.borderWidth = 1
+        
+        
+        
     }
 }
