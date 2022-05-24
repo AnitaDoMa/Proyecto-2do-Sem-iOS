b82d7d4d31dddd58daf99d9dd42c46f1395e823e
me.ana <consuelo.dominguezm@uanl.edu.mx>
2022-05-24 13:52:08 -0500

    add dropdown

--- Proyecto-2do-Sem-iOS/Mapas.swift
+++ Proyecto-2do-Sem-iOS/Mapas.swift
@@ -6,14 +6,50 @@
 //
 
 import UIKit
-import SwiftUI
 
 class Mapas: UIViewController {
+    
+    let maps = ["Seleccione un mapa", "FCB - General", "FCB - Unidad A - Primer Piso", "FCB - Unidad A - Segundo Piso", "FCB - Unidad A - Tercer Piso", "FCB - Unidad C", "FCB - Unidad D - Primer Piso", "FCB - Unidad D - Segundo Piso", "FCB - Unidad D - Tercer Piso"]
+    
+    @IBOutlet weak var chooseMapPickerView: UIPickerView!
+    
+    @IBOutlet weak var labelMap: UILabel!
+    
+    
+    @IBOutlet weak var mapImage: UIImageView!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-
+        chooseMapPickerView.delegate = self
+        chooseMapPickerView.dataSource = self
+        chooseMapPickerView.layer.backgroundColor = #colorLiteral(red: 0.7730752826, green: 0.9282240272, blue: 1, alpha: 1)
+        chooseMapPickerView.layer.borderWidth = 1
+        chooseMapPickerView.layer.borderColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        chooseMapPickerView.layer.cornerRadius = 8
+        
     }
 }
+
+
+extension Mapas: UIPickerViewDelegate, UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return maps.count
+    }
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return maps[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        labelMap.text = maps[row]
+        mapImage.image = UIImage(named: maps[row])
+        
+    }
+    
+}
+    
