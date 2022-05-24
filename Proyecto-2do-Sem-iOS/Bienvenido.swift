3f658ad1d989950de83253771f4552b373426e21
me.ana <consuelo.dominguezm@uanl.edu.mx>
2022-05-24 16:22:54 -0500

    Yaaaa

--- Proyecto-2do-Sem-iOS/Bienvenido.swift
+++ Proyecto-2do-Sem-iOS/Bienvenido.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class Bienvenido: UIViewController {
     
+    
     @IBOutlet var FCB: UIButton!
     override func viewDidLoad() {
     super.viewDidLoad()
@@ -18,6 +19,5 @@ class Bienvenido: UIViewController {
         FCB.layer.borderColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
         FCB.layer.borderWidth = 1
         FCB.layer.backgroundColor = #colorLiteral(red: 0.7730752826, green: 0.9282240272, blue: 1, alpha: 1)
-        
     }
 }
